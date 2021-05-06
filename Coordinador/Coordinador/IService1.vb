cd6d3464b6794b9051eff1033a51e7ea96c26131
Julen Clarke <julen_clarke@hotmail.com>
2021-05-06 13:15:16 +0200

    Añadiendo proyecto Coordinador.

---
+++ Coordinador/Coordinador/IService1.vb
@@ -0,0 +1,23 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the interface name "IService1" in both code and config file together.
+<ServiceContract()>
+Public Interface IService1
+
+    <OperationContract()>
+    Function ObtenerMedia(Asignatura As String) As Single
+
+    ' TODO: Add your service operations here
+
+End Interface
+
+' Use a data contract as illustrated in the sample below to add composite types to service operations.
+
+<DataContract()>
+Public Class CompositeType
+
+    <DataMember()>
+    Public Property BoolValue() As Boolean
+
+    <DataMember()>
+    Public Property StringValue() As String
+
+End Class
---
+++ Coordinador/Coordinador/Service1.svc.vb
@@ -0,0 +1,27 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the class name "Service1" in code, svc and config file together.
+' NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.vb at the Solution Explorer and start debugging.
+Imports System.Data.SqlClient
+
+Public Class Service1
+    Implements IService1
+
+    Public Sub New()
+    End Sub
+
+    Public Function ObtenerMedia(Asignatura As String) As Single Implements IService1.ObtenerMedia
+        Dim con As New SqlConnection("Server=tcp:hadsjclarke001.database.windows.net,1433;Initial Catalog=hadsjclarke001;Persist Security Info=False;User ID=julen_clarke@hotmail.com@hadsjclarke001;Password=jA268526ft;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+        Dim da As New SqlDataAdapter("select HEstimadas from TareasGenericas where CodAsig = '" + Asignatura + "'", con)
+        Dim ds As New DataSet
+        da.Fill(ds, "Media")
+        Dim suma As Integer = 0
+        Dim divisor As Integer = 0
+        For i As Integer = 0 To ds.Tables(0).Rows.Count - 1
+            Dim horas As Integer = Integer.Parse(ds.Tables(0).Rows(i)(0).ToString())
+            suma += horas
+            divisor += 1
+        Next
+        Dim mediahoras As Single = suma / divisor
+        Return mediahoras
+    End Function
+
+End Class
